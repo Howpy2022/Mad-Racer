8c914954f926ff66e5acb03379e732fcca4b23d2
Howard Kim <hyukim1102@gmail>
2023-07-24 13:54:31 -0400

    created Entity class

---
+++ client/src/utils/entity.py
@@ -0,0 +1,9 @@
+class Entity:
+    def __init__(self, x, y, width, height, surface, shape):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+        self.surface = surface
+        self.shape = shape
