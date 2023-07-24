c96448a486a34b304eda0ce6dee82a9700a62672
yds05238 <jy392@cornell.edu>
2023-07-24 14:00:21 -0400

    Updated entity class.

--- client/src/entities/entity.py
+++ client/src/entities/entity.py
@@ -1,9 +1,37 @@
 class Entity:
-    def __init__(self, x, y, width, height, surface, shape):
+    def __init__(self, config, x, y, width, height, surface, shape):
+        # Game Config 
+        self.config  = config 
+        # Position
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        
+        # Surface + Shape
         self.surface = surface
         self.shape = shape
+        
+    @property
+    def cx(self):
+        return self.x + self.width / 2
+
+    @property
+    def cy(self):
+        return self.y + self.height / 2
+
+    @property
+    def rect(self):
+        return self.shape 
+    
+        
+    def process(self):
+        # Process entity in 1 game tick 
+        pass 
+    
+    def draw(self):
+        # Draw entity on screen
+        pass 
+
+    def collide(self, other):
+        # Return true if entity collides with other entity
+        pass 
