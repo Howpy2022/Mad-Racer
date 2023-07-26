aff717cf0dc1a103d1efa60a86dfceaa2af9f203
yds05238 <jy392@cornell.edu>
2023-07-26 15:24:00 -0400

    Car moving. Turning not working.

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import sys
+import math 
 
 import pygame
+
+# Utils 
+def scale_image(img, new_width=None, new_height=None):
+    # Scales the pygame img to (new_width, new_height)
+    # At least one of new_width, new_height must be given
+    # If only one is given, the other is scaled to maintain aspect ratio
+    assert (new_width is not None or new_height is not None)
+    
+    if new_width is not None and new_height is None:
+        width, height = img.get_size()
+        ratio = new_width / width
+        new_height = int(height * ratio)
+    elif new_height is not None and new_width is None:
+        width, height = img.get_size()
+        ratio = new_height / height
+        new_width = int(width * ratio)
+    
+    size = round(new_width), round(new_height)
+    return pygame.transform.scale(img, size)
+
+
+def draw_image(win, img, top_left, angle):
+    # Draws the image with top_left positioning and angle rotation about the center 
+    rot_img = pygame.transform.rotate(img, angle)
+    new_rect = rot_img.get_rect(
+        center=img.get_rect(topleft=top_left).center)
+    win.blit(rot_img, new_rect.topleft)
+
 
 
 # Configuration
@@ -78,6 +107,7 @@ class Button():
 
 def myFunction():
     print('Button Pressed')
+    
 
 def handleToSettings():
     global screen_mode
@@ -89,7 +119,13 @@ def handleToMenu():
 
 def handleToStart():
     global screen_mode
-    screen_mode = "start"            
+    screen_mode = "start"
+    
+def handleToGame():
+    print("Button Pressed")
+    
+    global screen_mode
+    screen_mode = "game"            
 
 def start_screen():
     # Handle start screen
@@ -149,14 +185,126 @@ def settings_screen():
         fpsClock.tick(60)
 
 
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        draw_image(win, self.img, (self.x, self.y), self.angle)
+
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move_backward(self):
+        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+    def collide(self, mask, x=0, y=0):
+        # TODO: decelerate instead?
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+
+    def reset(self):
+        self.x, self.y = self.START_POS
+        self.angle = 0
+        self.vel = 0
+
+CAR_WIDTH = 20
+CAR_HEIGHT = 50
+RED_CAR = pygame.Surface((CAR_WIDTH, CAR_HEIGHT))
+RED_CAR.fill((255, 0, 0))
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
+        
+    def process(self, win=None):
+        # Handle display
+        if win is None:
+                self.draw(screen)
+        else:
+            self.draw(win)
+            
+        # Handle movement
+        keys = pygame.key.get_pressed()
+        moved = False
+
+        if keys[pygame.K_a]:
+            self.rotate(left=True)
+        if keys[pygame.K_d]:
+            self.rotate(right=True)
+        if keys[pygame.K_w]:
+            moved = True
+            self.move_forward()
+        if keys[pygame.K_s]:
+            moved = True
+            self.move_backward()
+
+        if not moved:
+            self.reduce_speed()
+            
+            
+
+
+def game_screen():
+    # Handle racing game screen
+    if screen_mode == "game":
+        
+        screen.fill((20, 20, 20))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        player_car.process()
+
+        pygame.display.flip()
+        fpsClock.tick(60)
+        
+        
+        
+
 customButton = Button(30, 100, 400, 100, 'Play', handleToMenu, False, "start")
 customButton = Button(30, 210, 400, 100, 'Settings', handleToSettings, False, "start")
 
 customButton = Button(30, 30, 400, 100, 'Close', handleToStart, False, "menu")
-customButton = Button(30, 140, 400, 100, 'Host', myFunction, False, "menu")
+customButton = Button(30, 140, 400, 100, 'Host', handleToGame, False, "menu")
 customButton = Button(30, 250, 400, 100, 'Join', myFunction, False, "menu")
 
 customButton = Button(30, 100, 400, 100, 'Close', handleToStart, False, "settings")
+
+player_car = PlayerCar(8, 8)
 
 # Game loop 
 while True:
@@ -168,6 +316,6 @@ while True:
     start_screen()
     menu_screen()
     settings_screen()
-    
+    game_screen()
     
 
