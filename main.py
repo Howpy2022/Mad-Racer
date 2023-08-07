e68398eef902b907ac85091681dad2a43420613d
Howpy2022 <hyukim1102@gmail.com>
2023-08-07 13:58:05 -0400

    Updated volume buttons

--- main.py
+++ main.py
@@ -333,7 +333,7 @@ customButton = Button(30, 140, 150, 50, 'Host', handleToGame, False, "menu")
 customButton = Button(30, 250, 150, 50, 'Join', myFunction, False, "menu")
 
 customButton = Button(30, 30, 150, 50, 'Close', handleToStart, False, "settings")
-customButton = Button(335, 180, 150, 50, 'On', handleToStart, False, "settings")
+customButton = Button(330, 180, 150, 50, 'On', handleToStart, False, "settings")
 customButton = Button(485, 180, 150, 50, 'Off', handleToStart, False, "settings")
 
 player_car = PlayerCar(8, 8)
