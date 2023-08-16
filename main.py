0da73e535b281ed36c9a59491503656ec7ca904e
Howpy2022 <hyukim1102@gmail.com>
2023-08-16 15:36:36 -0400

    Created Map Screen

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import math
 
@@ -223,6 +224,57 @@ def settings_screen():
         fpsClock.tick(60)
 
 
+class Maps(pygame.sprite.Sprite):
+    def __init__(self, tile_map, y, x, rot):
+        map_files = []
+        map_tile = ['X.png', 'I.png', 'L.png', 'T.png', '0.png', 'null.png']
+
+        crossing = 0
+        straight = 1
+        turn = 2
+        split = 3
+        deadend = 4
+        null = 5
+
+        map_1 = [
+                    [2, 1, 3, 1, 1, 3, 1, 1, 1, 4],
+                    [1, 5, 1, 5, 4, 0, 1, 2, 5, 4],
+                    [1, 4, 3, 1, 3, 3, 1, 3, 2, 1],
+                    [3, 1, 3, 1, 3, 5, 4, 5, 1, 1],
+                    [3, 2, 1, 5, 1, 5, 3, 1, 0, 3],
+                    [1, 2, 0, 1, 0, 3, 0, 4, 1, 1],
+                    [1, 5, 1, 4, 2, 1, 1, 2, 3, 1],
+                    [1, 2, 0, 1, 3, 3, 0, 0, 2, 1],
+                    [1, 1, 4, 2, 2, 5, 1, 2, 1, 3],
+                    [2, 3, 1, 3, 1, 1, 3, 1, 1, 2]
+        ]
+
+        map_1_rot = [
+                    [1, 1, 0, 1, 1, 0, 1, 1, 1, 3],
+                    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                    [0, 1, 2, 1, 0, 2, 1, 2, 0, 0],
+                    [1, 1, 0, 1, 3, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 1, 1, 0, 3],
+                    [0, 2, 0, 1, 0, 0, 0, 3, 0, 0],
+                    [0, 0, 0, 1, 3, 0, 0, 1, 3, 0],
+                    [0, 1, 0, 1, 0, 2, 0, 0, 3, 0],
+                    [0, 0, 2, 1, 3, 0, 0, 2, 1, 3],
+                    [2, 2, 1, 2, 1, 1, 2, 1, 1, 3]
+        ]
+
+        
+        pygame.sprite.Sprite.__init__(self)
+        self.image = map_files[tile_map]
+        self.rect = self.image.get_rect()
+
+        if rot != 0:
+            self.image = pygame.transform.rotate(self.image, r * 90)
+        
+        self.x = x
+        self.y = y
+
+    def update(self, cam_x, cam_y):
+        self.rect.topleft = self.x - cam_x, self.y - cam_y
 class AbstractCar:
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
@@ -324,8 +376,8 @@ class PlayerCar(AbstractCar):
 
         if not moved:
             self.reduce_speed()
-            
-            
+
+
 
 
 def game_screen():
